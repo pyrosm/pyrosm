36bc37bb6a2132f7d2a0c9b5cb16c1823e859017
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2020-04-07 16:53:19 +0100

    Add typing to make loops faster and use np arrays.

--- pyrosm/geometry.pyx
+++ pyrosm/geometry.pyx
@@ -3,11 +3,12 @@ from pygeos import linestrings
 import shapely
 
 cdef create_node_lookup_dict(nodes):
+    cdef int i
     ids = np.concatenate([group['id'] for group in nodes])
     lats = np.concatenate([group['lat'] for group in nodes])
     lons = np.concatenate([group['lon'] for group in nodes])
     coords = np.stack((lons, lats), axis=-1)
-    return {ids[i]: coords[i] for i in range(len(ids))}
+    return {ids[i]: coords[i] for i in range(0, len(ids))}
 
 cdef pygeos_to_shapely(geom):
     if geom is None:
@@ -20,16 +21,22 @@ cdef to_shapely(pygeos_array):
     out[:] = [pygeos_to_shapely(geom) for geom in pygeos_array]
     return out
 
-cdef create_way_geometries(nodes, ways):
+cdef _create_way_geometries(nodes, ways):
     cdef dict way
     cdef long long node
-    cdef list coords
+    cdef list coords, way_nodes
+    cdef int i, ii, nn, n=len(ways['id'])
+
+    # Lookup for all nodes that are available for given way
     lookup_dict = create_node_lookup_dict(nodes)
 
     geometries = []
-    for way in ways:
+    for i in range(0, n):
+        way_nodes = ways['nodes'][i]
         coords = []
-        for node in way['nodes']:
+        nn = len(way_nodes)
+        for ii in range(0, nn):
+            node = way_nodes[ii]
             try:
                 coords.append((lookup_dict[node][0], lookup_dict[node][1]))
             except:
@@ -43,3 +50,7 @@ cdef create_way_geometries(nodes, ways):
          if geom is not None else None
          for geom in geometries],
         dtype=object))
+
+cpdef create_way_geometries(nodes, ways):
+    return _create_way_geometries(nodes, ways)
+
