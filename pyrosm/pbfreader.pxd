11f9d29879122fd019daf0c244aa1ee6b716dab3
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2020-04-07 17:03:05 +0100

    Use Khash, c-array and cython tricks for fast lookups and loops. Add Relation parsing. Add Network filtering.

--- pyrosm/pbfreader.pxd
+++ pyrosm/pbfreader.pxd
@@ -1,6 +1,6 @@
-cdef parse_osm_data(filepath, bounding_box)
+cpdef parse_osm_data(filepath, bounding_box, exclude_relations)
+cpdef get_way_data(ways, way_tags_to_keep, network_filter)
 cdef get_primitive_blocks_and_string_tables(filepath)
 cdef parse_dense(pblock, data, string_table, bounding_box)
 cdef parse_nodes(pblock, data, bounding_box)
 cdef parse_ways(data, stringtable, node_lookup)
-cdef get_way_data(ways, way_tags_to_keep)
--- pyrosm/pbfreader.pyx
+++ pyrosm/pbfreader.pyx
@@ -2,15 +2,28 @@ from pyrosm.exceptions import PBFNotImplemented
 from struct import unpack
 import zlib
 from cafein_proto import BlobHeader, Blob, HeaderBlock, PrimitiveBlock
-from pyrosm.tagparser cimport tounicode, parse_dense_tags, parse_way_tags, explode_way_tags
+from pyrosm.tagparser cimport tounicode, parse_dense_tags, parse_tags, explode_way_tags
+from pyrosm.c_arrays cimport to_clong_array
+from cykhash.khashsets cimport Int64Set_from_buffer
+from cykhash import isin_int64
 import numpy as np
+from cpython cimport array
+from libc.stdlib cimport malloc, free
 
-cdef get_nodeid_lookup(nodes):
-    return np.sort(np.concatenate([group['id'].tolist() for group in nodes]))
+
+cdef get_nodeid_lookup_khash(nodes):
+    return Int64Set_from_buffer(
+        memoryview(
+            np.concatenate([group['id'].tolist()
+                            for group in nodes]).astype(np.int64)
+        )
+    )
+
 
 cdef get_primitive_blocks_and_string_tables(filepath):
     cdef int msg_len
     cdef bytes blob_data
+    cdef str feature
 
     f = open(filepath, 'rb')
 
@@ -140,11 +153,13 @@ cdef parse_dense(pblock, data, string_table, bounding_box):
                  tags=tags,
                  )]
 
+
 cdef parse_nodes(pblock, data, bounding_box):
     """
     bounding_box : list-like
         Coordinates that are used to filter data in format: [minx, miny, maxx, maxy]
     """
+
     ids = []
     versions = []
     changesets = []
@@ -194,43 +209,51 @@ cdef parse_nodes(pblock, data, bounding_box):
                 lat=lat
                 )
 
+
 cdef parse_nodeids_from_ref_deltas(refs):
-    cdef long long nid, delta
-    cdef int i
-    nodes = []
-    nid = 0
+    cdef long long delta, nid = 0
+    cdef int i, N = len(refs)
+    cdef long long * refs_c = to_clong_array(refs)
+    cdef long long *nodes = <long long *>malloc(N*sizeof(long long))
 
-    for delta in refs:
-        nid += delta
-        nodes.append(nid)
-    return nodes
+    try:
+        for i in range(N):
+            nid += refs_c[i]
+            nodes[i] = nid
+        return [x for x in nodes[:N]]
 
-
-def nodes_for_way_exist(self, nodes, node_lookup):
-        source = np.array(nodes)
-        try:
-            np.any(node_lookup[np.searchsorted(node_lookup, source)] == source)
-            return True
-        except:
-            return False
+    finally:
+        free(nodes)
+        free(refs_c)
 
 
-cdef parse_ways(data, stringtable, node_lookup):
-    cdef list way_set, nodes
+cdef nodes_for_way_exist_khash(nodes, node_lookup):
+    v = array.array('q', nodes)
+    cdef int n = len(v)
+
+    result = array.array('B', [False] * n)
+    isin_int64(v, node_lookup, result)
+    if True in result:
+        return True
+    return False
+
+
+cdef parse_ways(data, string_table, node_lookup):
     cdef long long id
-    cdef int version, timestamp
+    cdef int version, i, timestamp, n=len(data)
 
     way_set = []
-    for way in data:
+    for i in range(0, n):
+        way = data[i]
         nodes = parse_nodeids_from_ref_deltas(way.refs)
         if node_lookup is not None:
-            if nodes_for_way_exist(nodes, node_lookup):
+            if nodes_for_way_exist_khash(nodes, node_lookup):
                 way_set.append(
                     dict(
                         id=way.id,
                         version=way.info.version,
                         timestamp=way.info.timestamp,
-                        tags=parse_way_tags(way.keys, way.vals, stringtable),
+                        tags=parse_tags(way.keys, way.vals, string_table),
                         nodes=nodes,
                     )
                 )
@@ -240,21 +263,131 @@ cdef parse_ways(data, stringtable, node_lookup):
                     id=way.id,
                     version=way.info.version,
                     timestamp=way.info.timestamp,
-                    tags=parse_way_tags(way.keys, way.vals, stringtable),
+                    tags=parse_tags(way.keys, way.vals, string_table),
                     nodes=nodes,
                 )
             )
     return way_set
 
-cdef get_way_data(ways, way_tags_to_keep):
+
+cdef get_relation_members(relation, str_table):
+    relation_types = {
+        0: b"node",
+        1: b"way",
+        2: b"relation"
+        }
+
+    # Member-ids
+    member_N = len(relation.memids)
+    member_id_deltas = np.zeros(member_N+1, dtype=np.int64)
+    member_id_deltas[1:] = list(relation.memids)
+    member_ids = np.cumsum(member_id_deltas)[1:]
+
+    # Member types - 0: Node, 1: Way, 2: Relation
+    member_types = np.array([relation_types[t]
+                             for t in relation.types],
+                            dtype=object)
+
+    # Member roles
+    member_roles = np.array([str_table[role_id] for role_id in relation.roles_sid],
+                            dtype=object)
+
+    return dict(
+        member_id=member_ids,
+        member_type=member_types,
+        member_role=member_roles,
+        )
+
+
+cdef parse_relations(data, string_table):
+    N = len(data)
+
+    # Version
+    versions = np.array([rel.info.version for rel in data], dtype=np.int64)
+
+    # Ids (delta coded)
+    id_deltas = np.zeros(N+1, dtype=np.int64)
+    id_deltas[1:] = [rel.id for rel in data]
+    ids = np.cumsum(id_deltas)[1:]
+
+    # Timestamp
+    timestamps = np.array([rel.info.timestamp for rel in data], dtype=np.int64)
+
+    # Changeset
+    changesets = np.array([rel.info.changeset for rel in data], dtype=np.int64)
+
+    # Relation members
+    members = np.array([
+        get_relation_members(rel, string_table)
+        for rel in data],
+        dtype=object
+        )
+
+    # Tags
+    tags = np.array([
+            parse_tags(rel.keys, rel.vals, string_table)
+            for rel in data],
+            dtype=object
+            )
+
+    return [dict(id=ids,
+                version=versions,
+                changeset=changesets,
+                timestamp=timestamps,
+                members=members,
+                tags=tags,
+                )]
+
+
+cdef filter_network_data(data_records, data_filter):
+    cdef str rec_value
+    cdef int i, N=len(data_records)
+
+    filtered_data = []
+    filter_out = False
+
+    if data_filter is not None:
+        filter_keys = list(data_filter.keys())
+
+    for i in range(0, N):
+        record = data_records[i]
+        if "highway" not in record.keys():
+            continue
+        # Check if should be filtered based on given data_filter
+        if data_filter is not None:
+            for k, v in record.items():
+                if k in filter_keys:
+                    if v in data_filter[k]:
+                        filter_out = True
+                        break
+            if not filter_out:
+                filtered_data.append(record)
+            filter_out = False
+        else:
+            filtered_data.append(record)
+    return filtered_data
+
+
+cpdef get_way_data(ways, way_tags_to_keep, network_filter):
+    return _get_way_data(ways, way_tags_to_keep, network_filter)
+
+
+cdef _get_way_data(ways, way_tags_to_keep, network_filter):
     cdef int i
-    cdef str k, key
-    way_data, all_tag_keys = explode_way_tags(ways)
+
+    # ID and Node-information is still needed later
+    way_tags_to_keep += ["id", "nodes"]
+
+    # Filter data with given filter
+    # (if network_filter is None, will keep all with tag 'highway')
+    ways = filter_network_data(ways, network_filter)
+    cdef int n=len(ways)
 
     lookup = dict.fromkeys(way_tags_to_keep, None)
     data = {k: [] for k in way_tags_to_keep}
 
-    for i, way in enumerate(way_data):
+    for i in range(0, n):
+        way = ways[i]
         way_records = dict.fromkeys(way_tags_to_keep, None)
         for k, v in way.items():
             try:
@@ -268,19 +401,30 @@ cdef get_way_data(ways, way_tags_to_keep):
     # Convert to arrays
     arrays = {}
     for key, value_list in data.items():
-        # Keep tag only if it contains data
-        unique = list(set(value_list))
-        if len(unique) < 2:
-            if unique[0] is None:
-                continue
+        # Nodes are in a list and should always be kept
+        if not isinstance(value_list[0], list):
+            # Otherwise keep tag only if it contains data
+            unique = list(set(value_list))
+            if len(unique) < 2:
+                if unique[0] is None:
+                    continue
         arrays[key] = np.array(value_list, dtype=object)
-    return arrays, all_tag_keys
+    return arrays
 
-cdef parse_osm_data(filepath, bounding_box):
-    primitive_blocks, string_tables = get_primitive_blocks_and_string_tables(filepath)
+
+cpdef parse_osm_data(filepath, bounding_box,
+                     exclude_relations):
+    return _parse_osm_data(filepath, bounding_box, exclude_relations)
+
+
+cdef _parse_osm_data(filepath, bounding_box,
+                     exclude_relations):
     all_ways = []
     all_nodes = []
+    all_relations = []
     node_lookup_created = False
+
+    primitive_blocks, string_tables = get_primitive_blocks_and_string_tables(filepath)
 
     for pblock, str_table in zip(primitive_blocks, string_tables):
         for pgroup in pblock.primitivegroup:
@@ -292,11 +436,18 @@ cdef parse_osm_data(filepath, bounding_box):
                 # Once all the nodes have been parsed comes Ways
                 if bounding_box is not None:
                     if not node_lookup_created:
-                        node_lookup = get_nodeid_lookup(all_nodes)
+                        node_lookup = get_nodeid_lookup_khash(all_nodes)
                         all_ways += parse_ways(pgroup.ways, str_table, node_lookup)
                     else:
                         all_ways += parse_ways(pgroup.ways, str_table, node_lookup)
                 else:
                     all_ways += parse_ways(pgroup.ways, str_table, None)
+            elif len(pgroup.relations) > 0:
+                if exclude_relations:
+                    continue
+                all_relations += parse_relations(pgroup.relations, str_table)
 
-    return all_nodes, all_ways
+    # Explode the way tags
+    all_ways, all_way_tag_keys = explode_way_tags(all_ways)
+
+    return all_nodes, all_ways, all_relations, all_way_tag_keys
