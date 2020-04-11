7709e33a18ff163970407b1c5165e2a29d00b165
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2020-04-11 13:15:22 +0100

    Use pygeos to make points

--- pyrosm/frames.pyx
+++ pyrosm/frames.pyx
@@ -1,14 +1,15 @@
 import pandas as pd
 import geopandas as gpd
-from pyrosm._arrays cimport concatenate_dicts_or_arrays
+from pyrosm._arrays cimport concatenate_dicts_of_arrays
+from pyrosm.geometry cimport _create_point_geometries
 
 
 cpdef create_nodes_gdf(node_dict_list):
-    nodes = concatenate_dicts_or_arrays(node_dict_list)
+    nodes = concatenate_dicts_of_arrays(node_dict_list)
     df = pd.DataFrame()
     for k, v in nodes.items():
         df[k] = v
-    df['geometry'] = gpd.points_from_xy(nodes['lon'], nodes['lat'])
+    df['geometry'] = _create_point_geometries(nodes['lon'], nodes['lat'])
     return gpd.GeoDataFrame(df, crs='epsg:4326')
 
 
