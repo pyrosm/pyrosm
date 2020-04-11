1e650f1eccbdcd464e91d715f301437f9c5ac7dd
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2020-04-11 13:16:05 +0100

    Rename and reorganize

--- pyrosm/networks.pyx
+++ pyrosm/networks.pyx
@@ -1,6 +1,5 @@
-import numpy as np
-from pyrosm.data_filter cimport filter_osm, get_filtered_data
-from pyrosm._arrays cimport convert_to_array_dict
+from pyrosm.data_filter cimport filter_osm
+from pyrosm._arrays cimport convert_to_arrays_and_drop_empty, convert_way_records_to_lists
 
 cdef filter_network_data(data_records, data_filter):
     return filter_osm(data_records, data_filter,
@@ -9,13 +8,14 @@ cdef filter_network_data(data_records, data_filter):
 
 cdef _get_way_data(ways, tags_to_keep, network_filter):
 
+    # Tags to keep as separate columns
     tags_to_keep += ["id", "nodes", "timestamp", "version"]
 
     # Filter data with given filter
     # (if network_filter is None, will keep all with tag 'highway')
     ways = filter_network_data(ways, network_filter)
-    data = get_filtered_data(ways, tags_to_keep)
-    arrays = convert_to_array_dict(data)
+    data = convert_way_records_to_lists(ways, tags_to_keep)
+    arrays = convert_to_arrays_and_drop_empty(data)
     return arrays
 
 
