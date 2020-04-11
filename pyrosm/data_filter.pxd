0a3926a76ff937e2f0c926a37ce16d9b1a36d750
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2020-04-11 13:13:12 +0100

    Reorganize codes and allow osm_data_type to match with a slice of string, add docs.

--- pyrosm/data_filter.pxd
+++ pyrosm/data_filter.pxd
@@ -1,2 +1,2 @@
 cdef filter_osm(data_records, data_filter, osm_data_type, filter_type)
-cdef get_filtered_data(ways, tags_to_keep)
+cdef filter_array_dict_by_indices(array_dict, indices)
--- pyrosm/data_filter.pyx
+++ pyrosm/data_filter.pyx
@@ -1,5 +1,3 @@
-from rapidjson import dumps
-
 class Solver:
     """Solver is used to toggle between exclude / keep checks applied in data filter."""
     def __init__(self, direction):
@@ -25,11 +23,47 @@ class Solver:
         return self.solver(value, container)
 
 
+cdef has_osm_data_type(osm_data_type, tag_keys):
+    cdef str key
+    cdef int i, n=len(tag_keys)
+    for i in range(0, n):
+        key = tag_keys[i]
+        # Allow osm_data_type to match with a slice of string
+        # E.g. with buildings the "building" tag-key might be missing
+        # (due to incorrect tagging), but it can still be attached
+        # as part of other keys, such as "building:levels".
+        if osm_data_type in key:
+            return True
+    return False
+
+
 cdef filter_osm(data_records, data_filter, osm_data_type, filter_type):
     """
-    osm_data_type can be: 'highway', 'building', or 'landuse'
-    filter_type can be: 'keep', 'exclude' 
-    (determines if the values in data_filter should be used for including/excluding data)
+    Filter OSM data records by given OSM tag key:value pairs.
+    
+    Parameters
+    ----------
+    data_records : list:
+        A list of OSM data records. 
+        A single record is a dictionary with OSM data, such as: 
+           - {"highway": "primary", "maxspeed": 80, "name": "Highway-name-foo", 
+              "nodes": [1111,2222,3333,4444]}
+              
+    data_filter : dict ( {"osm-key": ["list-of-osm-values"]} )
+        A dictionary of tag-keys and associated values that will be used to
+        filter the OSM data records, e.g. {"building": ["residential"]} filters
+        the records where "building" key has a value "residential". 
+        The records will be kept or excluded according the <filter_type> parameter.   
+              
+    osm_data_type : str
+        Basic data type is used for filtering, such as:
+         - 'highway' (for roads), 
+         - 'building' (for buildings), 
+         - 'landuse' (for landuse) etc.
+         
+    filter_type : str ( 'keep' | 'exclude' ) 
+        Whether the given data_filter should 'keep' or 'exclude' the records 
+        where given tag:value pair is present in the record.  
     """
     cdef str rec_value
     cdef int i, N=len(data_records)
@@ -44,7 +78,7 @@ cdef filter_osm(data_records, data_filter, osm_data_type, filter_type):
 
     for i in range(0, N):
         record = data_records[i]
-        if osm_data_type not in record.keys():
+        if not has_osm_data_type(osm_data_type, list(record.keys())):
             continue
         # Check if should be filtered based on given data_filter
         if data_filter is not None:
@@ -61,29 +95,5 @@ cdef filter_osm(data_records, data_filter, osm_data_type, filter_type):
     return filtered_data
 
 
-cdef get_filtered_data(ways, tags_to_keep):
-    cdef int i
-    cdef int n=len(ways)
-
-    lookup = dict.fromkeys(tags_to_keep, None)
-    data = {k: [] for k in tags_to_keep}
-    data["tags"] = []
-
-    for i in range(0, n):
-        way = ways[i]
-        way_records = dict.fromkeys(tags_to_keep, None)
-        other_tags = {}
-        for k, v in way.items():
-            try:
-                # Check if tag should be kept as a column
-                lookup[k]
-                way_records[k] = v
-            except:
-                # If not add into tags
-                other_tags[k] = v
-        [data[k].append(v) for k, v in way_records.items()]
-        if len(other_tags) > 0:
-            data["tags"].append(dumps(other_tags))
-        else:
-            data["tags"].append(None)
-    return data
+cdef filter_array_dict_by_indices(array_dict, indices):
+    return {k: v[indices] for k, v in array_dict.items()}
