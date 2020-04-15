ffc571080dba038f1eebe1e9edcc48faa43b23b1
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2020-04-15 18:30:32 +0100

    Pumb version

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ requirements = [
 
 setup(
     name='pyrosm',
-    version='0.3.1',
+    version='0.3.2',
     license='MIT',
     description='A Python tool to parse OSM data from Protobuf format into GeoDataFrame.',
     long_description=read_long_description(),
