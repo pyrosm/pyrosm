e455254e68036a36f2a93d600f8c91f7885db9fc
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2020-04-15 12:00:21 +0100

    Ensure frame is built correctly always (avoid block error)

--- pyrosm/frames.pyx
+++ pyrosm/frames.pyx
@@ -13,11 +13,17 @@ cpdef create_nodes_gdf(node_dict_list):
     return gpd.GeoDataFrame(df, crs='epsg:4326')
 
 
-cpdef create_gdf(data_records, geometry_array):
-    datasets = [v for v in data_records.values()]
-    keys = list(data_records.keys())
+cpdef create_gdf(data_arrays, geometry_array):
     df = pd.DataFrame()
-    for i, key in enumerate(keys):
-        df[key] = datasets[i]
+
+    for key, data in data_arrays.items():
+        # When inserting nodes,
+        # those should be converted
+        # to lists to avoid block error
+        if key == "nodes":
+            df[key] = data.tolist()
+        else:
+            df[key] = data
+
     df['geometry'] = geometry_array
     return gpd.GeoDataFrame(df, crs='epsg:4326')
