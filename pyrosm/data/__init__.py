8043d27f000c635b4057e06b9086f7dfb64f84a2
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2020-04-18 15:54:20 +0100

    Add more test datasets

--- pyrosm/data/__init__.py
+++ pyrosm/data/__init__.py
@@ -16,13 +16,25 @@ _temp_files = {"helsinki_region_pbf":
                     "url": "https://gist.github.com/HTenkanen/"
                            "02dcfce32d447e65024d93d39ddb1812/"
                            "raw/5fe7ffb625f091591d8c29128a9e3b37870a5012/"
-                           "Helsinki_region.osm.pbf"}
+                           "Helsinki_region.osm.pbf"},
+               "new_york_state_pbf":
+                   {"name": "new-york-latest.osm.pbf",
+                    "url": "http://download.geofabrik.de/north-america/us/new-york-latest.osm.pbf"
+                    },
+               "greater_london_pbf":
+                   {"name": "greater-london-latest.osm.pbf",
+                    "url": "http://download.geofabrik.de/europe/"
+                           "great-britain/england/greater-london-latest.osm.pbf"
+                   },
+               "southern_california_pbf":
+                   {"name": "socal-latest.osm.pbf",
+                    "url": "http://download.geofabrik.de/"
+                           "north-america/us/california/socal-latest.osm.pbf"}
                }
 
 available = list(_package_files.keys()) + list(_temp_files.keys())
 
-
-def get_path(dataset):
+def get_path(dataset, update=False):
     """
     Get the path to the data file.
 
@@ -31,13 +43,18 @@ def get_path(dataset):
     dataset : str
         The name of the dataset. See ``pyrosm.data.available`` for
         all options.
+
+    update : bool
+        Whether the PBF file should be downloaded/updated if the dataset
+        with the same name exists in the temp.
     """
     if dataset in _package_files:
         return os.path.abspath(os.path.join(_module_path, _package_files[dataset]))
     elif dataset in _temp_files:
         # For large datasets, download and fetch from temp
         return download(url=_temp_files[dataset]["url"],
-                        filename=_temp_files[dataset]["name"])
+                        filename=_temp_files[dataset]["name"],
+                        update=update)
     else:
         msg = "The dataset '{data}' is not available. ".format(data=dataset)
         msg += "Available datasets are {}".format(", ".join(available))
