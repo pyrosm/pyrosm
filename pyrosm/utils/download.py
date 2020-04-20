b28bae06cffbef27e7334a034cafafa10ae9d3c6
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2020-04-20 20:13:01 +0100

    Round and use MB as default

--- pyrosm/utils/download.py
+++ pyrosm/utils/download.py
@@ -4,8 +4,8 @@ import os
 import enum
 
 
-# Enum for size units
-class SIZE_UNIT(enum.Enum):
+
+class UNIT(enum.Enum):
     BYTES = 1
     KB = 2
     MB = 3
@@ -13,21 +13,19 @@ class SIZE_UNIT(enum.Enum):
 
 
 def convert_unit(size_in_bytes, unit):
-    """ Convert the size from bytes to other units like KB, MB or GB"""
-    if unit == SIZE_UNIT.KB:
+    if unit == UNIT.KB:
         return size_in_bytes / 1024
-    elif unit == SIZE_UNIT.MB:
+    elif unit == UNIT.MB:
         return size_in_bytes / (1024 * 1024)
-    elif unit == SIZE_UNIT.GB:
+    elif unit == UNIT.GB:
         return size_in_bytes / (1024 * 1024 * 1024)
     else:
         return size_in_bytes
 
 
-def get_file_size(file_name, size_type=SIZE_UNIT.BYTES):
-    """ Get file in size in given unit like KB, MB or GB"""
+def get_file_size(file_name, size_type=UNIT.MB):
     size = os.path.getsize(file_name)
-    return convert_unit(size, size_type)
+    return round(convert_unit(size, size_type), 1)
 
 
 def download(url, filename, update):
@@ -49,7 +47,7 @@ def download(url, filename, update):
     # Download data to temp if it does not exist or if update is requested
     if update or file_exists is False:
         filepath, msg = urllib.request.urlretrieve(url, filepath)
-        filesize = get_file_size(filepath, SIZE_UNIT.MB)
+        filesize = get_file_size(filepath)
         print(f"Downloaded Protobuf data '{os.path.basename(filepath)}' "
               f"({filesize} MB) to TEMP:\n'{filepath}'")
     return filepath
