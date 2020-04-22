036ec34a0fffd4765e5957c4d409adbe3076ddde
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2020-04-22 16:26:53 +0100

    More robust check for creating linestrings instead of polygons.

--- pyrosm/relations.pyx
+++ pyrosm/relations.pyx
@@ -3,13 +3,30 @@ from pyrosm.geometry cimport pygeos_to_shapely, create_relation_geometry
 from pyrosm._arrays cimport convert_to_arrays_and_drop_empty, convert_way_records_to_lists
 from pygeos import multipolygons
 import numpy as np
+import geopandas as gpd
 
-cdef get_ways_for_relation(member_ids, building_relation_ways):
+cdef get_ways_for_relation(member_ids, member_roles, building_relation_ways):
+    # Ensure there are no duplicate member_ids/roles
+    member_ids, idx = np.unique(member_ids, return_index=True)
+    member_roles = member_roles[idx]
+
     mask = np.isin(building_relation_ways["id"], member_ids)
+
     # If data for relation is not available, skip
-    if len(member_ids) != np.sum(mask):
-        return None
-    return filter_array_dict_by_indices_or_mask(building_relation_ways, mask)
+    if np.sum(mask) == 0:
+        return None, None, None
+
+    # If some of the ways were missing, use the ones that are available.
+    # Might cause incorrectly shaped geometry but it's better than not having
+    # the data for relation at all
+    elif len(member_ids) != np.sum(mask):
+        # Filter member_ids and roles accordingly
+        member_mask = np.isin(member_ids, building_relation_ways["id"])
+        member_ids = member_ids[member_mask]
+        member_roles = member_roles[member_mask]
+
+    ways = filter_array_dict_by_indices_or_mask(building_relation_ways, mask)
+    return member_ids, member_roles, ways
 
 cdef get_relations(relations, relation_ways, node_coordinates):
     cdef int i, j, n2, m_cnt, n = len(relations["id"])
@@ -21,18 +38,22 @@ cdef get_relations(relations, relation_ways, node_coordinates):
         geometries = []
         tag_keys = list(rel["tags"][0].keys())
 
-        boundary = False
-        if "boundary" in tag_keys:
-            boundary = True
-
         # There might be multiple features part of the same relation
         n2 = len(rel["members"])
         for j in range(0, n2):
             member_ids = rel["members"][j]["member_id"]
             member_roles = rel["members"][j]["member_role"]
 
+            boundary = False
+            if "boundary" in tag_keys \
+                    or "route" in tag_keys \
+                        or member_ids[0] == member_ids[-1]:
+                boundary = True
+
             # Get ways for given relation
-            ways = get_ways_for_relation(member_ids, relation_ways)
+            member_ids, member_roles, ways = get_ways_for_relation(member_ids,
+                                                                   member_roles,
+                                                                   relation_ways)
 
             if ways is None:
                 continue
@@ -79,6 +100,11 @@ cdef _prepare_relations(relations, relation_ways, node_coordinates, tags_to_keep
 
     # Also geometries are parsed in this step
     relation_records = get_relations(relations, relation_ways, node_coordinates)
+
+    # Return empty frame if no relation records were successfully parsed
+    if len(relation_records) == 0:
+        return gpd.GeoDataFrame()
+
     data = convert_way_records_to_lists(relation_records, tags_to_keep)
     arrays = convert_to_arrays_and_drop_empty(data)
     return arrays
