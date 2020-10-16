cac859c87ecf2566feea5ccc5d9e15d151a45b66
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2020-10-16 10:45:38 +0300

    Add test for parsing nodes and edges

--- tests/test_network_parsing.py
+++ tests/test_network_parsing.py
@@ -300,3 +300,42 @@ def test_adding_extra_attribute(helsinki_pbf):
     assert extra_col in extra.columns
     assert len(extra[extra_col].dropna().unique()) > 0
     assert isinstance(gdf, GeoDataFrame)
+
+
+def test_getting_nodes_and_edges(test_pbf):
+    from pyrosm import OSM
+    from geopandas import GeoDataFrame
+    from shapely.geometry import Point, LineString
+
+    osm = OSM(filepath=test_pbf)
+
+    nodes, edges = osm.get_network(nodes=True)
+    orig_edges = osm.get_network()
+
+    # Edges with nodes and without nodes should be the same
+    assert edges.shape == orig_edges.shape
+
+    nodes = nodes.reset_index(drop=True)
+
+    assert isinstance(edges, GeoDataFrame)
+    assert isinstance(edges.loc[0, 'geometry'], LineString)
+
+    assert isinstance(nodes, GeoDataFrame)
+    assert isinstance(nodes.loc[0, 'geometry'], Point)
+
+    # Test shape
+    assert edges.shape == (265, 23)
+    assert nodes.shape == (382, 8)
+
+    # Edges should have "u" and "v" columns
+    required = ["u", "v"]
+    ecols = edges.columns
+    for col in required:
+        assert col in ecols
+
+    # Nodes should have "id", "x", and "y" columns
+    required = ["id", "x", "y"]
+    ncols = nodes.columns
+    for col in required:
+        assert col in ncols
+
