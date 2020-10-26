fb33a0d31956ccec0632ec012123041b260d4a6e
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2020-10-26 09:07:10 +0200

    GEOS compatibility warning comes from geopandas, so no need to repeat

--- pyrosm/utils/_compat.py
+++ pyrosm/utils/_compat.py
@@ -5,13 +5,6 @@ import warnings
 # shapely has something like: "3.6.2-CAPI-1.10.2 4d2925d6"
 # pygeos has something like: "3.6.2-CAPI-1.10.2"
 if not shapely_geos_version.startswith(geos_capi_version_string):
-    warnings.warn(
-        "The Shapely GEOS version ({}) is incompatible with the GEOS "
-        "version PyGEOS was compiled with ({}). The tool will work "
-        "but it runs a bit slower.".format(
-            shapely_geos_version, geos_capi_version_string
-        )
-    )
     PYGEOS_SHAPELY_COMPAT = False
 else:
     PYGEOS_SHAPELY_COMPAT = True
