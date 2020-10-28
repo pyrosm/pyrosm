080ee63dae082a09475077329b6e4ae4af6c6082
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2020-10-28 17:59:25 +0200

    Remove directionality logic from graph building functions

--- pyrosm/graph_export.pxd
+++ pyrosm/graph_export.pxd
@@ -1,3 +1,4 @@
-cpdef _create_igraph(nodes, edges, direction, from_id_col, to_id_col, force_bidirectional)
-cpdef _create_nxgraph(nodes, edges, direction, from_id_col, to_id_col, force_bidirectional)
+cpdef _create_igraph(nodes, edges, from_id_col, to_id_col, node_id_col)
+cpdef _create_nxgraph(nodes, edges, from_id_col, to_id_col, node_id_col)
+cpdef generate_directed_edges(edges, direction, from_id_col, to_id_col, force_bidirectional)
 
--- pyrosm/graph_export.pyx
+++ pyrosm/graph_export.pyx
@@ -1,15 +1,19 @@
 from pyrosm.utils._compat import HAS_IGRAPH, HAS_NETWORKX
 from pyrosm.config import Conf
+import geopandas as gpd
 
 # The values used to determine oneway road in OSM
 oneway_values = Conf.oneway_values
 
 cpdef _create_igraph(nodes,
                      edges,
-                     direction,
                      from_id_col,
                      to_id_col,
-                     force_bidirectional):
+                     node_id_col):
+    """
+    Creates a iGraph from directed edges and nodes.
+    NOTE: Assumes that the input edges GeoDataFrame is directed.  
+    """
     if not HAS_IGRAPH:
         raise ImportError("'python-igraph' needs to be installed "
                           "in order to export the network for igraph.")
@@ -25,24 +29,22 @@ cpdef _create_igraph(nodes,
 
     edge_list = []
 
-    edge_columns = edges.columns.to_list()
     n_edges = len(edges)
     n_nodes = len(nodes)
-    n_cols = len(edge_columns)
+
+    # Add columns for sequential ids
+    edges[from_id_int] = None
+    edges[to_id_int] = None
 
     # Convert edges to dict
     edges = edges.to_dict(orient="list")
-
-    # Add columns for sequential ids
-    edges[from_id_int] = [None for x in range(n_edges)]
-    edges[to_id_int] = [None for x in range(n_edges)]
 
     # Node-ids needs to be sequential for igraph
     nodes = nodes.reset_index(drop=True)
     nodes["node_id"] = nodes.index
 
     # Prepare dictionary for fast lookups
-    node_dict = {k: v for k, v in zip(nodes["id"].to_list(), nodes["node_id"].to_list())}
+    node_dict = {k: v for k, v in zip(nodes[node_id_col].to_list(), nodes["node_id"].to_list())}
 
     # Node attributes
     node_attributes = nodes.to_dict(orient='list')
@@ -70,53 +72,12 @@ cpdef _create_igraph(nodes,
         except Exception as e:
             raise e
 
-        # Oneway streets
-        if edges[direction][i] in oneway_values and not force_bidirectional:
-            # When travelling is allowed only against digitization direction
-            # flip the order of link nodes
-            if edges[direction][i] in ['-1', 'T']:
+        # Add sequential ids to edge_list
+        edge_list.append([from_seq_id, to_seq_id])
 
-                edge_list.append([to_seq_id, from_seq_id])
-                edges[from_id_int][i] = to_seq_id
-                edges[to_id_int][i] = from_seq_id
-
-            # In other cases add edge along the digitization direction
-            else:
-                edge_list.append([from_seq_id, to_seq_id])
-                edges[from_id_int][i] = from_seq_id
-                edges[to_id_int][i] = to_seq_id
-
-        # Roundabouts are oneways
-        elif 'junction' in edge_columns \
-                and edges['junction'][i] == 'roundabout' \
-                and not force_bidirectional:
-
-            edge_list.append([from_seq_id, to_seq_id])
-            edges[from_id_int][i] = from_seq_id
-            edges[to_id_int][i] = to_seq_id
-
-        else:
-            # If road is bi-directional add it in both ways
-            # ---------------------------------------------
-            # Along
-            edge_list.append([from_seq_id, to_seq_id])
-            edges[from_id_int][i] = from_seq_id
-            edges[to_id_int][i] = to_seq_id
-
-            # Against - Flip the link nodes
-            edge_list.append([to_seq_id, from_seq_id])
-
-            # Append the opposite direction link nodes and attributes
-            edges[from_id_int].append(to_seq_id)
-            edges[to_id_int].append(from_seq_id)
-            edges[from_id_col].append(to_node_id)
-            edges[to_id_col].append(from_node_id)
-
-            for key in edge_columns:
-                # Skip edge nodes which were added separately (in opposite direction)
-                if key == from_id_col or key == to_id_col:
-                    continue
-                edges[key].append(edges[key][i])
+        # Update the edge attributes
+        edges[from_id_int][i] = from_seq_id
+        edges[to_id_int][i] = to_seq_id
 
     del node_dict
 
@@ -128,10 +89,13 @@ cpdef _create_igraph(nodes,
 
 cpdef _create_nxgraph(nodes,
                       edges,
-                      direction,
                       from_id_col,
                       to_id_col,
-                      force_bidirectional):
+                      node_id_col):
+    """
+    Creates a NetworkX graph from directed edges and nodes.
+    NOTE: Assumes that the input edges GeoDataFrame is directed.
+    """
     if not HAS_NETWORKX:
         raise ImportError("'networkx' needs to be installed "
                           "in order to export the network for networkx / osmnx.")
@@ -144,14 +108,13 @@ cpdef _create_nxgraph(nodes,
 
     crs = edges.crs
     n_edges = len(edges)
-    oneway_values = ['yes', 'true', '1', '-1', 'T', 'F']
     edge_list = []
 
     # Convert edges to dict
     edge_attributes = edges.to_dict(orient="index")
 
     # Prepare node dictionary for fast lookups
-    node_dict = {k: None for k in nodes["osmid"].to_list()}
+    node_dict = {k: None for k in nodes[node_id_col].to_list()}
 
     # Node attributes
     node_attributes = nodes.to_dict(orient="index")
@@ -187,41 +150,7 @@ cpdef _create_nxgraph(nodes,
 
         # Create edges
         # ------------
-
-        e_attributes = edge_attributes[i]
-
-        # Oneway streets
-        if edge_attributes[i][direction] in oneway_values and not force_bidirectional:
-            # When travelling is allowed only against digitization direction
-            # flip the order of link nodes
-            if edge_attributes[i][direction] in ['-1', 'T']:
-                e_attributes["u"] = to_node_id
-                e_attributes["v"] = from_node_id
-                edge_list.append([to_node_id, from_node_id, 0, e_attributes])
-
-            # In other cases add edge along the digitization direction
-            else:
-                edge_list.append([from_node_id, to_node_id, 0, e_attributes])
-
-        # Roundabouts are oneways
-        elif 'junction' in edge_attributes[i].keys() \
-                and edge_attributes[i]['junction'] == 'roundabout' \
-                and not force_bidirectional:
-
-            edge_list.append([from_node_id, to_node_id, 0, e_attributes])
-
-        else:
-
-            # If road is bi-directional add it in both ways
-            # ---------------------------------------------
-
-            # Along
-            edge_list.append([from_node_id, to_node_id, 0, e_attributes])
-
-            # Append the opposite direction link nodes and attributes
-            e_attributes["u"] = to_node_id
-            e_attributes["v"] = from_node_id
-            edge_list.append([to_node_id, from_node_id, 0, e_attributes])
+        edge_list.append([from_node_id, to_node_id, 0, edge_attributes[i]])
 
     del node_dict
 
@@ -230,6 +159,68 @@ cpdef _create_nxgraph(nodes,
     graph.add_nodes_from(node_attributes)
     graph.add_edges_from(edge_list)
     graph.graph["crs"] = crs
-    graph.graph["name"] = "NetworkX graph"
+    graph.graph["name"] = "Made with Pyrosm library."
 
     return graph
+
+cpdef generate_directed_edges(edges,
+                              direction,
+                              from_id_col,
+                              to_id_col,
+                              force_bidirectional):
+    """
+    Generates directed set of edges from network 
+    following rules specified in 'direction' column.
+    
+    If 'force_bidirectional=True' travel to both direction is allowed for all edges.
+    """
+    cdef int i, n_edges = len(edges)
+
+    # Convert edges to dict
+    edge_attributes = edges.to_dict(orient="records")
+
+    # Generate directed set of edges
+    for i in range(0, n_edges):
+
+        # Get nodeids for the edge
+        # ------------------------
+        from_node_id = edge_attributes[i][from_id_col]
+        to_node_id = edge_attributes[i][to_id_col]
+
+        # Create edges
+        # ------------
+
+        # Oneway streets
+        if edge_attributes[i][direction] in oneway_values and not force_bidirectional:
+            # When travelling is allowed only against digitization direction
+            # flip the order of link nodes
+            if edge_attributes[i][direction] in ['-1', 'T']:
+                edge_attributes[i]["u"] = to_node_id
+                edge_attributes[i]["v"] = from_node_id
+
+            # Do nothing if travel is allowed along the digitization direction
+            else:
+                continue
+
+        # Roundabouts are oneways
+        elif 'junction' in edge_attributes[i].keys() \
+                and edge_attributes[i]['junction'] == 'roundabout' \
+                and not force_bidirectional:
+            # Do nothing
+            continue
+
+        else:
+
+            # If road is bi-directional add the "against" direction
+            # -----------------------------------------------------
+            # Take a deepcopy with dict-comprehension,
+            # so that from_node_id and to_node_id attribute info stays correct
+            # in the dictionary. This is needed only with bi-directional road segments.
+            e_attributes = {k: v for k, v in edge_attributes[i].items()}
+
+            # Append the opposite direction link nodes and attributes
+            e_attributes["u"] = to_node_id
+            e_attributes["v"] = from_node_id
+            edge_attributes.append(e_attributes)
+
+    return gpd.GeoDataFrame(edge_attributes, geometry="geometry", crs=edges.crs)
