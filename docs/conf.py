1ab5d883935f6212c790415deb7d65201c59f01f
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2020-11-18 14:31:32 +0200

    Remove unnecessary templating

--- docs/conf.py
+++ docs/conf.py
@@ -94,8 +94,7 @@ master_doc = 'index'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-
+#html_static_path = ["_static"]
 #html_css_files = ['css/custom.css']
 
 # The name of the Pygments (syntax highlighting) style to use.
